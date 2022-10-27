0405fec72dedc7bf09ccd4366b966752fbee1c11
hectoreg03 <96084225+hectoreg03@users.noreply.github.com>
2022-10-26 20:55:21 -0500

    Add files via upload

--- mailchimp/app.js
+++ mailchimp/app.js
@@ -1,18 +1,18 @@
 const express= require('express');
 const app= express();
 const mailchimp = require("@mailchimp/mailchimp_marketing");
+const https=require('https');
 
-const apiKey="97a6cef27db1492e133cb9b5da4bb21a-us14";
+const apiKey="de2e12a51bfb3eee20899ef1949e2b07-us14";
 const list_id="8c68d36d69";
 mailchimp.setConfig({
-  apiKey: "97a6cef27db1492e133cb9b5da4bb21a-us14",
-  server: "us14",
+  apiKey: "de2e12a51bfb3eee20899ef1949e2b07-us14",
+  server: "us14"
 });
 
 async function run() {
   const response = await mailchimp.ping.get();
   console.log(response);
-  var url= "https://us14.api.mailchimp.com/3.0/lists/"+list_id;
 }
 
 
@@ -21,7 +21,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended:true }));
 app.get('/', (req,res)=>{
     res.sendFile(__dirname+"/signup.html");
-})
+});
 
 app.post('/',(req,res)=>{
     var fname= req.body.fName;
@@ -31,8 +31,46 @@ app.post('/',(req,res)=>{
     console.log(lname);
     console.log(email);
     console.log("escritqewro");
+    
+    var data={members: [{
+      email_address:email, 
+      status:"subscribed",
+      merge_fields:{
+        FNAME:fname,
+        LNAME:lname
+      }
+    }]};
+    var url= "https://us14.api.mailchimp.com/3.0/lists/"+list_id;
+    options={method:"POST", auth:"Hector Eguiarte:"+apiKey};
+    const mailRequest= https.request(url,options,(response)=>{
+      console.log(response);
+      //Copie la parte del response, no entendia las instrcciones
+      if(response.statusCode === 200) {
+        response.on("data", (data) => {
+            var jsonResp = JSON.parse(data);
+            if(jsonResp["error_count"] === 0) {
+                res.render(__dirname + "/success.html");
+            } else {
+                res.render(__dirname + "/failure.html" );
+                console.log(jsonResp.errors[0]["error_code"]);
+                console.log(jsonResp.errors[0]["error"]);
+            }
+        });
+        response.on("error", (e) => {
+            res.render(__dirname + "/failure.html");
+        });
+    } else {
+        res.render(__dirname + "/failure.html");
+    }
+    });
+    mailRequest.write(JSON.stringify(data),()=>{});
+    mailRequest.end();
     res.sendFile(__dirname+"/success.html");
-})
+});
+
+app.post('/c',(req,res)=>{
+  res.redirect("/");
+});
 
 app.listen(3000,()=>{
     console.log("listening to port 3000");
